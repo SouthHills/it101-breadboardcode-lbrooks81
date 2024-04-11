a8b57b5538a71daf37635d799491144d2a0846b9
lbrooks81 <lbrooks81@southhills.edu>
2024-04-11 15:29:37 -0400

    LED Lab

--- 01_Blink/Blink.py
+++ 01_Blink/Blink.py
@@ -1,26 +1,30 @@
 from gpiozero import LED as LEDClass # Alias
 import time
 
-LED = LEDClass(17)  # define led
+LED_17 = LEDClass(17)  # define led
+LED_18 = LEDClass(18)
 
 def loop():
-    global LED
+    global LED_17
     while True:
-        LED.on() 
+        LED_17.on()
+        LED_18.off() 
         print ("led turned on >>>") # print information on terminal
         time.sleep(1)
-        LED.off()
+        LED_17.off()
+        LED_18.on()
         print ("led turned off <<<")
         time.sleep(1)
         
 def destroy():
-    global LED
+    global LED_17
     # Release resources
-    LED.close()
+    LED_17.close()
+    LED_18.close()
 
 if __name__ == "__main__":    # Program start point
     print("Program is starting ... \n")
-    print(f"Using pin {LED.pin}")
+    print(f"Using pin {LED_17.pin}")
     try:
         loop()
     except KeyboardInterrupt:   # Press ctrl-c to end the program.
