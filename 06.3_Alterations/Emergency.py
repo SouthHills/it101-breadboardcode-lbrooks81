cb4c1fea7d3ff47d0ae024109eb6804ad7a32eca
lbrooks81 <lbrooks81@southhills.edu>
2024-04-24 15:13:01 -0400

    Messsage (Ctrl + Enter to commit...)

--- 06.3_Alterations/Emergency.py
+++ 06.3_Alterations/Emergency.py
@@ -1,4 +1,4 @@
-from gpiozero import TonalBuzzer, Button
+from gpiozero import TonalBuzzer, Button, PWMLED
 import time
 from signal import pause
 
@@ -6,38 +6,41 @@ BUZZER = TonalBuzzer(17)
 BUTTON = Button(18)
 HIGH_TONE = 440 # The max is 880 but that hurts my ears
 LOW_TONE = 220
-    
+LED = PWMLED(26)
+
 def setup():
     # Setup events for when the button is pressed and released
-    BUTTON.when_pressed = alertor
-    BUTTON.when_released = stop_alertor
+    BUTTON.when_pressed = loop
+    BUTTON.when_released = stop_emergency
 
-def alertor():
-    print ('alertor turned on >>> ')
-    
+def loop():
+    global LED
+    print ('Emergency!')
+
     while True:  
         # Linear
         for x in range(LOW_TONE, HIGH_TONE):
             BUZZER.play(x)
             time.sleep(0.002)
-            
+            LED.value = x / 1000
             if not BUTTON.is_pressed:
                 return  
             
         for x in range(HIGH_TONE, LOW_TONE, -1):
             BUZZER.play(x)
             time.sleep(0.002)
-            
+            LED.value = x / 1000
             if not BUTTON.is_pressed:
                 return 
         
-def stop_alertor():
+def stop_emergency():
     BUZZER.stop()
-    print ('alertor turned off <<<')
+    print ('Emergency over')
 
 def destroy():
     BUZZER.close()
     BUTTON.close()
+    LED.close()
 
 if __name__ == '__main__':     # Program entrance
     print ('Program is starting...')
